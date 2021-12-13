90e65b43476629506687c5db82121014de5d347a
MuhammetErcan <ercanmuhammet667@gmail.com>
2021-12-13 23:39:17 +0300

    day 21

--- src/gun21/_05_MethodsExample.java
+++ src/gun21/_05_MethodsExample.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class _05_MethodsExample {
     public static void main(String[] args) {
-       /*
+        /*
          Aşağıdaki gibi bir menü çıkartarak kullanicidan sürekli değişkenleri alınız ve
          her bir menü elemanını değer alan bir fonksiyon yazınız.
             1-toplama
@@ -14,20 +14,27 @@ public class _05_MethodsExample {
             5-Faktöryel
             6-Çıkış
          Bir işlem seçiniz
-        */
-        menuYaz();
-        islemYap(1);
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        int secim;
+        do {
+            menuYaz();
+            System.out.println("Islem secin");
+            secim = scanner.nextInt();
+            islemYap(secim);
+        }while (secim<6);
 
     }
 
     public static void menuYaz() {
         System.out.println("***Menu***");
         System.out.println("1-toplama");
-        System.out.println("2-cıkarma");
-        System.out.println("3-carpma");
+        System.out.println("2-çıkarma");
+        System.out.println("3-çarpma");
         System.out.println("4-bolme");
-        System.out.println("5-faktoriyel");
-        System.out.println("6-çıkış");
+        System.out.println("5-Faktöryel");
+        System.out.println("6-Çıkış");
     }
 
     public static void islemYap(int islem) {
@@ -35,44 +42,64 @@ public class _05_MethodsExample {
         int num1, num2;
         switch (islem) {
             case 1:
-                System.out.println("1.sayiyi girin");
+                System.out.println("1. sayiyi girin");
                 num1 = scanner.nextInt();
                 System.out.println("2. sayiyi girin");
                 num2 = scanner.nextInt();
-                toplam(num1, num2);
+                toplam(num1,num2);
                 break;
             case 2:
-                System.out.println("1.sayiyi girin");
+                System.out.println("1. sayiyi girin");
                 num1 = scanner.nextInt();
                 System.out.println("2. sayiyi girin");
                 num2 = scanner.nextInt();
-                cikarma(num1, num2);
+                cikarma(num1,num2);
                 break;
             case 3:
-                System.out.println("1.sayiyi girin");
+                System.out.println("1. sayiyi girin");
                 num1 = scanner.nextInt();
                 System.out.println("2. sayiyi girin");
                 num2 = scanner.nextInt();
-                carpma(num1, num2);
+                carpma(num1,num2);
+                break;
             case 4:
+                System.out.println("1. sayiyi girin");
+                num1 = scanner.nextInt();
+                System.out.println("2. sayiyi girin");
+                num2 = scanner.nextInt();
+                bolme(num1,num2);
+                break;
             case 5:
-            case 6:
+                System.out.println("Sayi girin");
+                num1=scanner.nextInt();
+                faktoriyel(num1);
+                break;
         }
-
+    }
+    public static void toplam(int num1,int num2){
+        System.out.println("toplam="+(num1+num2));
     }
 
-    public static void toplam(int num1, int num2) {
-        System.out.println("toplam= " + (num1 + num2));
+    public static void cikarma(int num1, int num2){
+        System.out.println("cikarma="+(num1-num2));
+    }
+    public static void carpma(int num1, int num2){
+        System.out.println("carpma="+(num1*num2));
+    }
+    public static void bolme(int num1, int num2){
+        if (num2>0)
+            System.out.println("bolme="+(num1/num2));
+        else
+            System.out.println("Tanimsiz");
+    }
+    public static void faktoriyel(int num1){
+
+        double faktoriyel=1;
+        for (int i = 1; i <= num1; i++) {
+            faktoriyel*=i;
+        }
+        System.out.println("faktoriyel = " + faktoriyel);
     }
 
-    public static void cikarma(int num1, int num2) {
-        System.out.println("cikarma=" + (num1 - num2));
-    }
-
-    public static void carpma(int num1, int num2) {
-        System.out.println("carpma =" + (num1 * num2));
-
-    }
 }
-
 
--- src/gun21/_06_MethodsExample.java
+++ src/gun21/_06_MethodsExample.java
@@ -1,29 +1,41 @@
 package gun21;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class _06_MethodsExample {
     public static void main(String[] args) {
-       /*
-         aaabbbfffgggtttdddd bu sekilde yazilmis bir kelimeyi düzenleyecek bir metot yazin.
+        /*
+         aaabbbfffgggtttdddd b bu sekilde yazilmis bir kelimeyi düzenleyecek bir metot yazin.
          ciktisi bu sekilde olacaktir -> 3a3b3f3g3t4d
-        */
-        String str = "aaabbbfffgggtttdddd";
-        String [] harfler = str.split("");
-        System.out.println(Arrays.toString(harfler));
-
-        for (int i = 0; i < harfler.length; i++) {
-            for (int j = 0; j < harfler[i].length(); j++) {
+         */
 
 
+        System.out.println(duzenlenmis("aaabbbfffgggtttddddb"));
+    }
+
+    public static String duzenlenmis(String str) {
+
+        String yeniHali = "";
+        // ifadeyi düzenledikten sonra atacagimiz bos string degisken
+        int count; // cümle icerisindeki 1 harfin kac tane oldugunu bulmak icin degisken
+        for (int i = 0; i < str.length(); i++) {
+            // dis döngü sirayla harfi alir
+            String harf = String.valueOf(str.charAt(i)); // o an ki harfi asagida kontrol etmek icin string degiskene atadik
+            count=0; // her harf icin sayac 0 dan saymaya baslasin diye döngü icerisinde count u 0 a esitledik
+            for (int j = 0; j < str.length(); j++) {
+                // yukaridan gelen harfi cümle icerisinde kontrol etmek icin olan döngü
+                if (str.charAt(i)==str.charAt(j)){
+                    // yukaridan gelen harfi cümle(kelime icerisinde sirayla olup olmadigini kontrol ediyor)
+                    count++;
+                }
             }
+            if (!yeniHali.contains(harf))
+                // o an ki harf in düzenlenen string icerisinde olup olmadigini
+                // kontrol ediyor. Eger yoksa düzenlenen stringe ekliyor
+                yeniHali+=count+harf;
 
         }
-
-
-
+        return yeniHali;
     }
-}
 
+
+}
 

f80442fe55e1eca005f024690da7b6d4b4c7a665
MuhammetErcan <ercanmuhammet667@gmail.com>
2021-12-31 14:11:32 +0300

    gun 25 hs

--- src/gun25hs/_01ArrayList.java
+++ src/gun25hs/_01ArrayList.java
@@ -1,50 +1,52 @@
 package gun25hs;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class _01ArrayList {
+
     public static void main(String[] args) {
+
+
+        String str = "Java";
+        System.out.println(str);
+        str = str.substring(0, 2);
+        System.out.println(str.substring(0, 2));
+
+
+        int[] arr = new int[]{1,2,3};
+        arr[1] = 10;
+
+
+
+        System.out.println(arr);
+
+
+        double[] arr1 = new double[2];
+        String[] arr2 = new String[5];
+
+        Object[] arr3 = new Object[5];
+        arr3[0] = 1;
+        arr3[1] = "Java";
+        arr3[2] = new Scanner(System.in);
+
+
         /*
-        ratain all    --> ortak elemanlar
-
-        Collections.swap()  ---> list1 ed 1. index ile 4. index yerini değiştir.
+            Collections
+            List            Set
+            ArrayList       HashSet
+            LinkedList      LinkedHashSet
+                            TreeSet
+            Random Access
+            arr[2]
          */
-        /*
-        farklı elemanları bir liste at
-         */
-/*
-        ArrayList<Integer> list1 = new ArrayList<>(List.of(1,2,3,4,5,6,7,8,9));
-        ArrayList<Integer> list2 = new ArrayList<>(List.of(7, 8, 9, 10, 11, 12, 13));
-
-        list1.retainAll(list2);
 
 
-
-        list1.removeAll(list2);
-        System.out.println(list1);
-
- */
-        ArrayList<Integer> list1 = new ArrayList<Integer>(List.of(1,2,11,25,4,7,8,9));
-        int [] array = new int[list1.size()];
-        int enk=0;
-
-        for (int i = 0; i < list1.size(); i++) {
-
-
-            }
-        }
-
-
-
-
-
-
-
-
-
-
-
-
+        int a1 = 5;
+        var a2 = 5;
+        //var a3;   initialize edilmeden kullanilamaz
+        var sc = new Scanner(System.in);
 
     }
+
+}

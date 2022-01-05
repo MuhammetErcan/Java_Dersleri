f580fe5d972a11ee68c4513c45aa07b919de4059
MuhammetErcan <ercanmuhammet667@gmail.com>
2022-01-05 11:48:02 +0300

    gun 32

--- src/gun31/paket4/IslemReturnObject.java
+++ src/gun31/paket4/IslemReturnObject.java
@@ -21,10 +21,48 @@ public class IslemReturnObject {
         val -= x;
         return this;
     }
+    public IslemReturnObject carp(double x){
+        val*=x;
+        return this;
+    }
+
+    public IslemReturnObject bol(double x){
+        val/=x;
+        return this;
+    }
+    public IslemReturnObject mod(double x){
+        val%=x;
+        return this;
+    }
+    public IslemReturnObject power(double x){
+        val=Math.pow(val,x);
+        return this;
+    }
+    public IslemReturnObject karekok(){
+        val=Math.sqrt(val);
+        return this;
+    }
+    public IslemReturnObject convertToInt(){
+        val=(int)val;
+        return this;
+    }
+    public IslemReturnObject floor(){
+        val=Math.floor(val);
+        return this;
+    }
+    public IslemReturnObject ceil(){
+        val=Math.ceil(val);
+        return this;
+    }
+    public IslemReturnObject round(){
+        val=Math.round(val);
+        return this;
+    }
 
     public void yaz(){
         System.out.println("sonuc = " + val);
     }
+
 
     /*
         Ödev

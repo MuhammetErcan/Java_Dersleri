86523a55a1adb82e444cdb3d85a9dcfc63f5f1c7
MuhammetErcan <ercanmuhammet667@gmail.com>
2022-01-06 12:07:30 +0300

    gun 33

--- src/gun33/task2/Banka.java
+++ src/gun33/task2/Banka.java
@@ -3,47 +3,42 @@ package gun33.task2;
 public class Banka {
     int id;
     String subeAdi;
-    int kasaBakiyesi;
-
-    public Banka(int id, String subeAdi, int kasaBakiyesi) {
-        this.id = id;
-        this.subeAdi = subeAdi;
-        this.kasaBakiyesi = kasaBakiyesi;
-    }
+    int kasaBakiyesi = 0;
 
     public Banka(int id, String subeAdi) {
         this.id = id;
         this.subeAdi = subeAdi;
     }
 
-    public void paraYatir(int x){
-        if (x>0){
-            kasaBakiyesi+=x;
-        }
-        else
-            System.out.println("yanlış miktar girdiniz");
+    public Banka(int id, String subeAdi, int kasaBakiyesi) {
+        this(id, subeAdi);  // constructor cagir ilk satirda olmali
+        if (kasaBakiyesi>0)
+            this.kasaBakiyesi = kasaBakiyesi;
+    }
+
+    public boolean paraYatir(int yatirilacakPara){
+        if (yatirilacakPara>0) {
+            kasaBakiyesi += yatirilacakPara;
+            return true;
+        }else
+            return false;
+    }
+
+    public boolean paraCek(int cekilecekPara){
+        if (kasaBakiyesi >= cekilecekPara) {
+            kasaBakiyesi -= cekilecekPara;
+            return true;
+        }else
+            return false;
+
+    }
+
+    public int getBakiye(){
+        return kasaBakiyesi;
     }
 
     @Override
     public String toString() {
-        return "Banka{" +
-                "id=" + id +
-                ", subeAdi='" + subeAdi + '\'' +
-                ", kasaBakiyesi=" + kasaBakiyesi +
-                '}';
-    }
-
-    public void paraCek(int x){
-        if (x<kasaBakiyesi){
-            kasaBakiyesi-=x;
-        }else
-            System.out.println("Hesabınızda çekmek istediğiniz miktar bulunmamaktadır");
-    }
-
-    public static void paraGonder(Banka paraCekilecekBanka, Banka paraGidecekBanka, int miktar){
-        paraCekilecekBanka.kasaBakiyesi-=miktar;
-        paraGidecekBanka.kasaBakiyesi+=miktar;
-
-
+        return "Banka : " + subeAdi + ", Kasa Bakiyesi = " + kasaBakiyesi;
     }
 }
--- src/gun33/task2/Main.java
+++ src/gun33/task2/Main.java
@@ -1,22 +1,55 @@
 package gun33.task2;
 
 public class Main {
+    /*
+    Bir banka class'ı tanımlayınız. Veri kontrollerini ilgili yerlerde yapınız.
+    1.	3 adet field ekleyiniz. (id, subeAdi, kasaBakiyesi)
+    2.	initialize için 2 adet constructor kullanın.
+	3.	paraYatir(int yatirilacakPara); methodu ile bankaya para yatırın.
+	4.	paraCek();	methodu ile bankaya para çekin.
+	    getBakiye()
+    5.	Banka bilgilerini ekrana yazdıracak metodu oluşturun.
+        (Şube : Xbank, bakiye : xxx.xx TL)(toString)
+	6.	main'de 2 tane banka oluşturup para yatırma ve çekme işlemleri yapın
+	7.	olusturdugunuz iki banka arasinda para transferi yapin. (Çözüm düşüneniz nedir)
+     */
+
     public static void main(String[] args) {
-        Banka banka1=new Banka(10,"kadiköy",0);
-        Banka banka2=new Banka(11,"besiktas",0);
+        Banka b1 = new Banka(1, "A", 1000);
+        Banka b2 = new Banka(2, "B");
+        Banka b3 = new Banka(3, "C", -1000);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        /*
+        b2.paraYatir(500);
+        b3.paraYatir(300);
+        b1.paraCek(400);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println("-------------------");
+        paraTransferEt(b1, b3, 200);
+        paraTransferEt(b3, b2, 50);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+         */
 
-        banka1.paraYatir(30);
-        banka1.paraCek(5);
-        System.out.println(banka1.kasaBakiyesi);
-
-        banka2.paraYatir(25);
-        banka2.paraCek(5);
-        System.out.println(banka2.kasaBakiyesi);
-
-        Banka.paraGonder(banka1,banka2,5);
-        System.out.println(banka1.kasaBakiyesi);
-        System.out.println(banka2.kasaBakiyesi);
-
+        paraTransferEt(b1, b2, 100);
+        System.out.println(b1);
+        System.out.println(b2);
+        paraTransferEt(b1, b2, 100000);
+        System.out.println(b1);
+        System.out.println(b2);
     }
 
+    public static void paraTransferEt(Banka odeyecekBanka, Banka alacakBanka, int havaleMiktari){
+        if (odeyecekBanka.paraCek(havaleMiktari))
+            alacakBanka.paraYatir(havaleMiktari);
+        /*
+        if (odeyecekBanka.getBakiye()>havaleMiktari)
+            alacakBanka.paraYatir(havaleMiktari);
+         */
+    }
 }
